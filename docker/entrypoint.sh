09d3c1f4d5624fca24a1ca59c7d5d48d375587d8
weraleto <52561921+weraleto@users.noreply.github.com>
2021-09-21 22:39:35 +0300

    review #2 fixes

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-python manage.py migrate --no-input
-
-python manage.py collectstatic --no-input
-
-gunicorn --bind 0.0.0.0:80 movies_admin.wsgi
+gunicorn --bind 0.0.0.0:8000 movies_admin.wsgi
